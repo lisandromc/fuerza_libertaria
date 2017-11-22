5bec736cff09ef160f65ba4e1e87112fc5d18475
Pablo Vizcay <pablo.vizcay@gmail.com>
2017-11-22 16:32:57 -0300

    added config to debug puma logs

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :deploy_to, "/home/vizcay/applications/fuerza_libertaria"
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, "config/secrets.yml.key"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets"
--- config/puma/production.rb
+++ config/puma/production.rb
@@ -4,6 +4,7 @@ daemonize true
 pidfile "tmp/pids/puma.pid"
 state_path "tmp/pids/puma.state"
 plugin :tmp_restart
+stdout_redirect '/home/vizcay/applications/fuerza_libertaria/shared/log/puma_stdout', '/home/vizcay/applications/fuerza_libertaria/shared/log/puma_stderr', true
 
 on_worker_boot do
   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
