3547230e45e0a9ede7d6b13bbab92cc959f7754a
Pablo Vizcay <pablo.vizcay@gmail.com>
2017-11-21 22:35:56 -0300

    added ApplicationController#usuario_actual and as view helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :usuario_actual
+
+  protected
+
+  def usuario_actual
+    @usuario_actual ||= Usuario.find_by(id: session[:usuario_id])
+  end
 end
