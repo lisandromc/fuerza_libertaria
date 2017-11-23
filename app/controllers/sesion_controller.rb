30f3b38af6feab417a1740bc4cd7ad3c3f05f85e
Pablo Vizcay <pablo.vizcay@gmail.com>
2017-11-22 22:59:03 -0300

    improved restablecer password feature by checking empty email

--- app/controllers/sesion_controller.rb
+++ app/controllers/sesion_controller.rb
@@ -2,7 +2,7 @@ class SesionController < ApplicationController
   def iniciar
     @usuario = Usuario.find_by(email: params[:email])
     if @usuario&.password_digest.blank?
-      redirect_to root_path, alert: "El usuario no tiene un password establecido, por favor genere uno aquí."
+      redirect_to root_path, alert: "El usuario no tiene un password establecido, por favor utilizar la función de restablecer."
     elsif @usuario&.authenticate(params[:password])
       session[:usuario_id] = @usuario.id
       redirect_to root_path, notice: "Bienvenido #{@usuario.nombre}."
