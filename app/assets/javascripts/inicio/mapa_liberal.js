57f1ccd5d8ff916dc4eaad911ea769d79603776e
Pablo Vizcay <pablo.vizcay@gmail.com>
2017-12-16 15:09:08 -0300

    improved mapa liberal with info windows

--- app/assets/javascripts/inicio/mapa_liberal.js
+++ app/assets/javascripts/inicio/mapa_liberal.js
@@ -1,13 +1,31 @@
-$(function() {
-  if ($('body').data('action') != 'mapa_liberal')
-    return;
-
-  $('#map').css('height', $(window).height() - $('nav.navbar').height());
-
-  handler = Gmaps.build('Google');
-  handler.buildMap({ internal: {id: 'map'}}, function(){
-    var markers = handler.addMarkers(serverExports.locations);
-    handler.bounds.extendWith(markers);
-    handler.fitMapToBounds();
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: { lat: -37.1833, lng: -67.3667 }
   });
-});
+
+  serverExports.usuarios.forEach(function(usuario) {
+    var marker = new google.maps.Marker({
+      position: usuario.location,
+      map: map
+    });
+
+    if (usuario.infoWindow) {
+      var infowindow = new google.maps.InfoWindow({
+        content: usuario.infoWindow
+      });
+
+      marker.addListener('click', function() {
+        infowindow.open(map, marker);
+      });
+    }
+
+    // myMarker = new google.maps.Marker({
+    //   position: serverExports.myLocation,
+    //   map: map,
+    //   icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+    //   zIndex: 10000
+    // });
+
+  })
+}
