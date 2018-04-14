4f59f6f8ede1f2fc4c3a04732b23a2f07e70b0c8
Pablo Vizcay <pablo.vizcay@gmail.com>
2018-04-14 17:54:27 -0300

    combine puma output into one log

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -4,7 +4,7 @@ daemonize true
 pidfile "tmp/pids/puma.pid"
 state_path "tmp/pids/puma.state"
 plugin :tmp_restart
-stdout_redirect 'log/puma.out', 'log/puma.error', true
+stdout_redirect 'log/puma.log', 'log/puma.log', true
 
 on_worker_boot do
   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
